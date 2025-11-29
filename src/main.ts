7c84c1fa8446140c9627f59c206de44f9fbf8042
darkolol9 <darkolol9@gmail.com>
2025-11-29 09:26:14 +0200

    fixed

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = false;
-const controls = new OrbitControls(camera, renderer.domElement);
+new OrbitControls(camera, renderer.domElement);
 
 
 const axis = new AxisGrid(maxSegments, 1, 0x444444);
